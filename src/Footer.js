0aaa7d836a5ac028de3c6452bef033414eaea34c
otienotilen <otienotilen@gmail.com>
2024-03-29 12:11:45 +0300

    changes

--- src/Footer.js
+++ src/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           paddingRight: "100px",
         }}
       >
-        Copyright © 2022 laphricamedia. All Rights Reserved.
+        Copyright © 2022 Tilen. All Rights Reserved.
       </span>
     </div>
   );
